05fd7ca32f2475b44c40c56b9f76f101b79e2f81
Mila <Pod4asova20@gmail.com>
2022-04-26 10:48:27 +0300

    JavaCoreLesson07

---
+++ src/main/java/lesson7/Main.java
@@ -0,0 +1,8 @@
+package lesson7;
+
+public class Main {
+    public static void main(String[] args) {
+        UserIntefaceView userIntefaceView = new UserIntefaceView();
+        userIntefaceView.runInterface();
+    }
+}
